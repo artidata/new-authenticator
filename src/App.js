4fee89af22c70ac6fff9fd0c11b832ae81fda1d5
artidata <imaduddin.htm@gmail.com>
2021-12-21 05:31:23 +0000

    Working sample with the old authenticator.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { Amplify } from "aws-amplify";
+import { AmplifySignOut } from "@aws-amplify/ui-react-v1";
+import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import { AmplifyAuthenticator } from "@aws-amplify/ui-react-v1";
+import { Auth } from "aws-amplify";
 
-function App() {
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+export default function App() {
+  const [authState, setAuthState] = useState();
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  }, []);
+
+  if (authState === AuthState.SignedIn && user) {
+    return (
+      <div>
+        <h1>Basic Example</h1>
+        <h1>Hello {user.attributes.email}</h1>
+        <div style={{ width: "50px" }}>
+          <AmplifySignOut />
+        </div>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route
+              index
+              element={
+                <div>
+                  <h2>Home</h2>
+                </div>
+              }
+            />
+            <Route
+              path="about"
+              element={
+                <div>
+                  <h2>About</h2>
+                </div>
+              }
+            />
+            <Route
+              path="dashboard"
+              element={
+                <div>
+                  <h2>Dashboard</h2>
+                </div>
+              }
+            />
+          </Route>
+        </Routes>
+      </div>
+    );
+  } else {
+    return <AmplifyAuthenticator />;
+  }
+}
+
+function Layout() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+      <Outlet />
     </div>
   );
 }
-
-export default App;
