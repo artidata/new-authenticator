5fe1d319e81f71c29195b882c5a710ce8a52af2e
artidata <imaduddin.htm@gmail.com>
2022-02-21 16:57:52 +0000

    Seems to have been fixed now.

--- src/App.js
+++ src/App.js
@@ -1,86 +1,86 @@
 import { Amplify } from "aws-amplify";
-import {
-  Authenticator,
-  useAuthenticator,
-  withAuthenticator,
-} from "@aws-amplify/ui-react";
+import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
-import { Routes, Route, Outlet, Link } from "react-router-dom";
+import { Routes, Route, Outlet, Link, Navigate } from "react-router-dom";
 
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
-function App() {
-  const { user, signOut } = useAuthenticator();
+export default function App() {
+  const { user, signOut } = useAuthenticator((context) => [context.user]);
 
   if (user) {
     return (
-      <div>
+      <>
         <h1>Post Login</h1>
-        <h1>Hello {user.attributes.email}</h1>
+        <Routes>
+          <Route path="/" element={<LayoutPost />}>
+            <Route index element={<h2>Home</h2>} />
+            <Route path="about" element={<h2>About</h2>} />
+            <Route path="dashboard" element={<h2>Dashboard</h2>} />
+            <Route path="*" element={<Navigate replace to="/" />} />
+          </Route>
+        </Routes>
+        <div>Hello {user.attributes.email}</div>
         <button style={{ width: "50px" }} onClick={signOut}>
           Sign Out
         </button>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route
-              index
-              element={
-                <div>
-                  <h2>Home</h2>
-                </div>
-              }
-            />
-            <Route
-              path="about"
-              element={
-                <div>
-                  <h2>About</h2>
-                </div>
-              }
-            />
-            <Route
-              path="dashboard"
-              element={
-                <div>
-                  <h2>Dashboard</h2>
-                </div>
-              }
-            />
-          </Route>
-        </Routes>
-      </div>
+      </>
     );
   } else {
     return (
-      <div>
+      <>
         <h1>Pre Login</h1>
-        <Authenticator variation="modal" />
-      </div>
+        <Routes>
+          <Route path="/" element={<LayoutPre />}>
+            <Route index element={<h2>Home</h2>} />
+            <Route path="faq" element={<h2>FAQ</h2>} />
+            <Route path="contact-us" element={<h2>Contact Us</h2>} />
+            <Route path="*" element={<Navigate replace to="/" />} />
+          </Route>
+        </Routes>
+        <Authenticator />
+      </>
     );
   }
 }
 
-function Layout() {
+function LayoutPost() {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-        </ul>
-      </nav>
+    <>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
       <hr />
       <Outlet />
-    </div>
+    </>
   );
 }
 
-export default App;
+function LayoutPre() {
+  return (
+    <>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/faq">FAQ</Link>
+        </li>
+        <li>
+          <Link to="/contact-us">Contact Us</Link>
+        </li>
+      </ul>
+      <hr />
+      <Outlet />
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Authenticator } from "@aws-amplify/ui-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Authenticator.Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Authenticator.Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
