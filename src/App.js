296d46f469ecf20e81ccc5bad9f0e14d700f55e5
artidata <imaduddin.htm@gmail.com>
2022-02-21 16:25:02 +0000

    Simplify the reproducible example.

--- src/App.js
+++ src/App.js
@@ -21,123 +21,76 @@ export default function App() {
 
   if (authState === AuthState.SignedIn && user) {
     return (
-      <div>
+      <>
         <h1>Post Login</h1>
         <Routes>
           <Route path="/" element={<LayoutPost />}>
-            <Route
-              index
-              element={
-                <div>
-                  <h2>Home</h2>
-                </div>
-              }
-            />
-            <Route
-              path="about"
-              element={
-                <div>
-                  <h2>About</h2>
-                </div>
-              }
-            />
-            <Route
-              path="dashboard"
-              element={
-                <div>
-                  <h2>Dashboard</h2>
-                </div>
-              }
-            />
+            <Route index element={<h2>Home</h2>} />
+            <Route path="about" element={<h2>About</h2>} />
+            <Route path="dashboard" element={<h2>Dashboard</h2>} />
             <Route path="*" element={<Navigate replace to="/" />} />
           </Route>
         </Routes>
-        <h1>Hello {user.attributes.email}</h1>
+        <div>Hello {user.attributes.email}</div>
         <div style={{ width: "50px" }}>
           <AmplifySignOut />
         </div>
-        
-      </div>
+      </>
     );
   } else {
     return (
-      <div>
+      <>
         <h1>Pre Login</h1>
         <Routes>
           <Route path="/" element={<LayoutPre />}>
-            <Route
-              index
-              element={
-                <div>
-                  <h2>Home</h2>
-                </div>
-              }
-            />
-            <Route
-              path="faq"
-              element={
-                <div>
-                  <h2>FAQ</h2>
-                </div>
-              }
-            />
-            <Route
-              path="contact-us"
-              element={
-                <div>
-                  <h2>Contact Us</h2>
-                </div>
-              }
-            />
+            <Route index element={<h2>Home</h2>} />
+            <Route path="faq" element={<h2>FAQ</h2>} />
+            <Route path="contact-us" element={<h2>Contact Us</h2>} />
             <Route path="*" element={<Navigate replace to="/" />} />
           </Route>
         </Routes>
         <AmplifyAuthenticator />
-      </div>
+      </>
     );
   }
 }
 
 function LayoutPost() {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-        </ul>
-      </nav>
+    <>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
       <hr />
       <Outlet />
-    </div>
+    </>
   );
 }
 
 function LayoutPre() {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/faq">FAQ</Link>
-          </li>
-          <li>
-            <Link to="/contact-us">Contact Us</Link>
-          </li>
-        </ul>
-      </nav>
+    <>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/faq">FAQ</Link>
+        </li>
+        <li>
+          <Link to="/contact-us">Contact Us</Link>
+        </li>
+      </ul>
       <hr />
       <Outlet />
-    </div>
+    </>
   );
 }
